7429383576dfa04fbc69a701a9c5011cd18e0b96
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-03-16 15:31:14 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+def is_comprime(a,b):
+    if(a<b):
+        a,b = b,a
+    if(a == b or (a==1 or b==1) or a%b==0):
+        return False
+    while(a!=b):
+        a = a-b
+        if(a<b): a,b = b,a
+    return a == 1 and b == 1
+
+a, b = map(int, input().split())
+if(is_comprime(a,b)):
+    print("YES")
+else:
+    print("NO")
