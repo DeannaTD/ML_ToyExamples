1d204f7541b68b073d1e9064cf2e358763d19a74
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-03-16 15:56:03 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,12 @@
 def is_comprime(a,b):
-    pow2 = [2,4,8]
-    if(a in pow2 and b%2!=0):
-        return True
-    if(b in pow2 and a%2!=0):
-        return True
     if(a<b):
         a,b = b,a
     if(a == b or (a==1 or b==1) or a%b==0):
         return False
-    while(a!=b):
-        a = a-b
+    while(a%b != 0):
+        a = a % b
         if(a<b): a,b = b,a
-    return a == 1 and b == 1
+    return b == 1
 
 a, b = map(int, input().split())
 if(is_comprime(a,b)):
