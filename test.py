65fa28bd9f48cf6850b2d7e697788d83d6cb82f5
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-03-29 15:46:08 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-def Election(x,y,z):
-    if(x + y + z > 1):
+def Nor(x,y):
+    if (x == 0 and y == 0):
         return 1
-    else:
-        return 0
+    return 0
+
 
 N = int(input())
 a = []
 for i in range(N):
-    x,y,z = map(int, input().split())
-    a.append(Election(x,y,z))
+    x,y = map(int, input().split())
+    a.append(Nor(x,y))
 
 for i in a:
     print(i)
