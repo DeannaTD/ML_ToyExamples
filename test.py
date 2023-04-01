945347bf903780265eb271ebb001b36b10c7fe54
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-04-01 16:04:28 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-import random
-
-N, X = map(int, input().split())
-A = []
-ans = []
-for i in range(N):
-    A.append(random.randint(0, 5))
-    if(A[i] == X):
-        ans.append(i)
-
-print(" ".join(map(str, A)))
-for a in ans:
-    print(a)
+N = int(input())
+f= []
+for i in range(1, N+1):
+    s = str(i)
+    if(s.__contains__('0')):
+        continue
+    for j in s:
+        if i % int(j) != 0:
+            break
+    else:
+        f.append(i)
+print(" ".join(map(str,f)))
