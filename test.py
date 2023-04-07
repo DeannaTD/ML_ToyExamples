a5ca6bd6a76b937d8b0b6a90dd09a5d232e7a958
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-04-07 14:51:34 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 N = int(input())
 A, B = map(int, input().split())
+players = map(int, input().split())
 
 res = 0
 
-for i in range(N):
-    p = int(input())
+for p in players:
     if A <= p <= B:
         res += 1
 
