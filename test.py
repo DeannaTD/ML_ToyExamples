83a5c05084344d946a24245dc9fe124bc99d17c6
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-04-13 15:15:08 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,15 @@
-import random
-def printArray(array):
-    for i in range(len(array)-1):
-        print(array[i], end=" ")
-    print(array[len(array)-1])
+s = input()
+r = ""
+for i in range(len(s)):
+    if(s[i] == 'a'):
+        r += 'b'
+    elif(s[i] == 'b'):
+        r += 'a'
+    elif(s[i] == 'A'):
+        r += 'B'
+    elif(s[i] == 'B'):
+        r += 'A'
+    else:
+        r += s[i]
 
-N = int(input())
-
-arr = []
-
-for i in range(N):
-    arr.append(random.randint(0,5))
-
-printArray(arr)
-
-inds = []
-
-for i in range(N):
-    if(i in inds):
-        continue
-    if (arr.count(arr[i]) > 1):
-        for j in range(i, N):
-            if(arr[i] == arr[j]):
-                inds.append(j)
-
-if(len(inds) == 0):
-    print("NO")
-else:
-    print("YES")
-
-for ind in inds:
-    print(ind)
+print(r)
