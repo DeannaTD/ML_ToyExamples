abb90558ec311933839802b888d44cc247593180
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-04-13 15:51:07 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,5 @@
 s = input()
-words = []
-word = ""
-n = False
-
-for c in s:
-    if(c != ' ' and n == False):
-        word += c
-        n = True
-    elif(c == ' ' and n == True):
-        words.append(word)
-        word = ""
-        n = False
-    elif(c != ' '):
-        word += c
-
-mw = max(words, key=len)
-print(mw)
-print(len(mw))
+words = s.split(' ')
+word = max(words, key=len)
+print(word)
+print(len(word))
