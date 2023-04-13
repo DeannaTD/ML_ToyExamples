c2505d56f15dd830ecd9da87d62b3d69bb0245ca
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-04-13 15:38:36 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 s = input()
-words = 0
+words = []
+word = ""
 n = False
 
 for c in s:
     if(c != ' ' and n == False):
-        words += 1
+        word += c
         n = True
     elif(c == ' ' and n == True):
+        words.append(word)
+        word = ""
         n = False
+    elif(c != ' '):
+        word += c
 
-print(words)
+i = 0
+for j in range(len(words)):
+    if(len(words[i]) < len(words[j])):
+        i = j
+print(words[i])
+print(len(words[i]))
