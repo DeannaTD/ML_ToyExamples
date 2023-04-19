02ac1fc9445842f5dc2371e5ae0ec4fb63252393
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-04-19 15:36:41 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-number = input()
+number = int(input())
 
-result = ""
-for i in range(1, len(number)-1):
-    result += number[i]
-
-if(int(result) == 0):
-    print(int(result))
-else:
-    print("")
+for n in range(1, number + 1):
+    s_n = str(n)
+    div = True
+    for d in s_n:
+        if(d == '0'):
+            div = False
+            break
+        if(n % int(d) != 0):
+            div = False
+            break
+    if(div):
+        print(n)
