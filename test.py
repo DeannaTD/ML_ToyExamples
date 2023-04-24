359bd9b2d040f498c2af5e5d4401479c8f6c79ca
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-04-24 15:36:41 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
-number = int(input())
+import re
 
-def div(n):
-    s_n = set(str(n))
-    for d in s_n:
-        if(d == "0"):
-            return False
-        if(n % int(d) != 0):
-            return False
-    return True
+def rtd(s):
+    romans = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    result = 0
+    for i in range(len(s)-1):
+        if romans[s[i]] < romans[s[i+1]]:
+            result -= romans[s[i]]
+        else:
+            result += romans[s[i]]
+    result = result + romans[s[-1]]
+    return result
 
-if(number != 0):
-    print("1", end="")
+s = input()
 
-for n in range(2, number+1):
-    if(div(n)):
-        print("", n, end="")
+r = re.findall(r'[I|V|X|L|C|D|M]+', s)
+
+for rn in r:
+    s = s.replace(rn, str(rtd(rn)))
+print(s)
