9725ab08cb9afebba6bc0e3b6fd5473bb1d71649
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-04-24 15:41:24 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,9 @@
-import re
-
-def rtd(s):
-    romans = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-    result = 0
-    for i in range(len(s)-1):
-        if romans[s[i]] < romans[s[i+1]]:
-            result -= romans[s[i]]
-        else:
-            result += romans[s[i]]
-    result = result + romans[s[-1]]
-    return result
-
-s = input()
-
-r = re.findall(r'[I|V|X|L|C|D|M]+', s)
-
-for rn in r:
-    s = s.replace(rn, str(rtd(rn)))
-print(s)
+N = input()
+if(len(N) % 2 == 1):
+    print("unlucky")
+else:
+    m = len(N) // 2
+    if(sum(map(int, N[:m])) == sum(map(int, N[m:]))):
+        print("lucky")
+    else:
+        print("unlucky")
