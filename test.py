815c57be788945408521e0410abe6b2c55cdb4b7
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-04-26 15:29:56 +0600

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,26 @@
-s = input()
-sign = ""
-t = []
-if(s.__contains__("+")):
-    sign = "+"
-    t = s.split("+")
-else:
-    sign = "-"
-    t = s.split("-")
+import re
 
-if(sign == "+"):
-    print(int(t[0]) + int(t[1]))
-else:
-    print(int(t[0]) - int(t[1]))
+def rtd(s):
+    romans = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    result = 0
+    for i in range(len(s)-1):
+        if romans[s[i]] < romans[s[i+1]]:
+            result -= romans[s[i]]
+        else:
+            result += romans[s[i]]
+    result = result + romans[s[-1]]
+    return result
+
+s = input()
+
+r = re.finditer(r'[I|V|X|L|C|D|M]+', s)
+n = next(r, False)
+
+while(n):
+    start = n.span()[0]
+    end = n.span()[1]
+    s = s[:start] + str(rtd(s[start:end])) + s[end:]
+    r = re.finditer(r'[I|V|X|L|C|D|M]+', s)
+    n = next(r, False)
+
+print(s)
