f12f35445d31360c76b665b49d29347090bbcc21
Diana Tolubaeva <33951437+DeannaTD@users.noreply.github.com>
2023-05-10 15:42:55 +0600

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ s = s.replace('Oo', 'u').replace('oo', 'u')
 s = s.replace('Ee', 'i').replace('ee', 'i')
 s = s.replace('Th', 'Z').replace('th', 'z')
 
-r = re.finditer(r'(.)\1', s)
+r = re.finditer(r'([bcdfghjklmnpqrstvwxz])\1', s)
 n = next(r, False)
 while n:
     s = s.replace(n.group(), n.group()[0])
